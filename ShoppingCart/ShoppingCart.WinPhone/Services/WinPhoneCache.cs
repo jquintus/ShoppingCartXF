66f8bc56ddd25c2df4f9a10dc41bdbf610f06ad1
jquintus <josh.quintus@live.com>
2014-09-29 23:46:23 -0400

    Adding cache for windows phone.

---
+++ ShoppingCart/ShoppingCart.WinPhone/Services/WinPhoneCache.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingCart.Services;
+using System;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.WinPhone.Services
+{
+    public class WinPhoneCache : ICache
+    {
+        private readonly IsolatedStorageSettings _settings;
+
+        public WinPhoneCache()
+        {
+            _settings = IsolatedStorageSettings.ApplicationSettings;
+        }
+
+        public Task<T> GetObject<T>(string key)
+        {
+            if (!_settings.Contains(key))
+            {
+                return Task.FromResult((T)_settings[key]);
+            }
+            else
+            {
+                return Task.FromResult(default(T));
+            }
+        }
+
+        public Task InsertObject<T>(string key, T value)
+        {
+            if (!_settings.Contains(key))
+            {
+                _settings.Add(key, value);
+            }
+            else
+            {
+                _settings[key] = value;
+            }
+            _settings.Save();
+
+            return Task.FromResult(0);
+        }
+    }
+}
